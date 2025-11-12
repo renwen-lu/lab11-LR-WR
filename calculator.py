63bc62968ee028d1da8d4ce0be598357bc9720d1
renwen-lu <itsmeoliver68@gmail.com>
2025-11-12 18:47:43 -0500

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,26 @@
 """
 calculator.py
+# https://github.com/renwen-lu/lab11-LR-WR
+# Partner 1: Renwen Lu
+# Partner 2: Ruijin Wang
 - Defines functions used to create a simple calculator
 
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
-    pass
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        raise ValueError("Cannot divide by zero.")
+    return x / y
+
 
 
