0f1c274ef76ddf3a53eb8039f849add33d70e8eb
renwen-lu <itsmeoliver68@gmail.com>
2025-11-12 19:46:55 -0500

    calculator.py

--- calculator.py
+++ calculator.py
@@ -3,18 +3,10 @@
 # Partner 2: Ruijin Wang
 import math
 
-def square_root(a):
-    if a < 0:
-        raise ValueError("Cannot take square root of a negative number")
-    return math.sqrt(a)
-
-def hypotenuse(a, b):
-    return math.hypot(a, b)
-
 def add(a, b):
     return a + b
 
-def subtract(a, b):
+def sub(a, b):
     return a - b
 
 def mul(a, b):
@@ -29,7 +21,7 @@ def exp(a, b):
     return a ** b
 
 
-def logarithm(base, x):
-    if base <= 0 or x <= 0:
+def log(a, b):
+    if a <= 0 or b <= 0:
         raise ValueError("Invalid input for logarithm")
-    return math.log(x, base)
+    return math.log(b, a)
