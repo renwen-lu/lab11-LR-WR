882ea8bef563aecbe1a49b42d3727cafef38c34b
renwen-lu <itsmeoliver68@gmail.com>
2025-11-12 19:36:59 -0500

    test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -20,32 +20,32 @@ class TestCalculator(unittest.TestCase):
             div(5, 0)
 
 
-    class TestCalculator(unittest.TestCase):
+class TestCalculator(unittest.TestCase):
         ######### Partner 2
-        def test_add(self): 
-            self.assertEqual(add(2, 3), 5)
-            self.assertEqual(add(-1, 4), 3)
-            self.assertEqual(add(0, 0), 0)
+    def test_add(self): 
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 4), 3)
+        self.assertEqual(add(0, 0), 0)
 
-        def test_subtract(self): 
-            self.assertEqual(sub(5, 3), 2)
-            self.assertEqual(sub(0, 7), -7)
-            self.assertEqual(sub(-2, -3), 1)
+    def test_subtract(self): 
+        self.assertEqual(sub(5, 3), 2)
+        self.assertEqual(sub(0, 7), -7)
+        self.assertEqual(sub(-2, -3), 1)
 
-        def test_log_invalid_argument(self):
-            with self.assertRaises(ValueError):
-                log(-2, 8)
+    def test_log_invalid_argument(self):
+        with self.assertRaises(ValueError):
+        log(-2, 8)
 
-        def test_hypotenuse(self):
-            self.assertEqual(hypotenuse(3, 4), 5)
-            self.assertEqual(hypotenuse(5, 12), 13)
-            self.assertAlmostEqual(hypotenuse(1.5, 2.0), math.hypot(1.5, 2.0))
+    def test_hypotenuse(self):
+        self.assertEqual(hypotenuse(3, 4), 5)
+        self.assertEqual(hypotenuse(5, 12), 13)
+        self.assertAlmostEqual(hypotenuse(1.5, 2.0), math.hypot(1.5, 2.0))
 
-        def test_sqrt(self):
-            self.assertEqual(square_root(9), 3)
-            self.assertAlmostEqual(square_root(2.25), 1.5)
-            with self.assertRaises(ValueError):
-                square_root(-1)
+    def test_sqrt(self):
+        self.assertEqual(square_root(9), 3)
+        self.assertAlmostEqual(square_root(2.25), 1.5)
+        with self.assertRaises(ValueError):
+        square_root(-1)
 
     # Do not touch this
     if __name__ == "__main__":
